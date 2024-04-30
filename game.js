27f391d5bebb4a04bff1b686b8a590c1cdad64b4
MariyaN23 <sellaite505@gmail.com>
2024-04-30 20:04:04 +0300

    settings, status, players

--- game.js
+++ game.js
@@ -1,6 +1,54 @@
 class Game {
-    test() {
-        return 23
+    #settings
+    #status = 'pending'
+    #player1
+    #player2
+
+    constructor() {
+        this.#player1 = new Player(NumberUtil.getRandomNumber(0, this.#settings.gridSize.x),
+            NumberUtil.getRandomNumber(0, this.#settings.gridSize.y))
+        const randomPositionForPlayer2 = getRandomPosition()
+        this.#player2 = new Player(randomPositionForPlayer2.x, randomPositionForPlayer2.y)
+    }
+    #getRandomPosition() {
+        let newX
+        let newY
+        do {
+            newX = NumberUtil.getRandomNumber(0, this.#settings.gridSize.x)
+            newY = NumberUtil.getRandomNumber(0, this.#settings.gridSize.y)
+        } while (newX === this.#player1.x && newY === this.#player1.y)
+        return {x: newX, y: newY}
+    }
+    set settings(settings) {
+        if (!settings.gridSize) {
+            throw new Error('Incorrect setting object')
+        }
+        this.#settings = settings
+    }
+    get settings() {
+        return this.#settings
+    }
+    get status() {
+        return this.#status
+    }
+    start() {
+        if (this.#status === 'pending') {
+            this.#status = 'in-progress'
+        }
+
+    }
+}
+
+class Player {
+    constructor(x, y) {
+        this.x = x
+        this.y = y
+    }
+}
+
+class NumberUtil {
+    static getRandomNumber(min, max) {
+        return Math.floor(Math.random()*(max - min + 1)) + min
     }
 }
 
--- game.test.js
+++ game.test.js
@@ -1,7 +1,23 @@
 const {Game} = require ('./game')
 
 describe('game test', ()=> {
-    it ('first test', ()=> {
+    it ('settings test', ()=> {
         const game = new Game()
+        game.settings = {
+            gridSize: {
+                x: 5,
+                y: 4
+            }
+        }
+        const settings = game.settings
+        expect(settings.gridSize.y).toBe(4)
+        expect(settings.gridSize.x).toBe(5)
+    })
+
+    it ('start game test', async ()=> {
+        const game = new Game()
+        expect(game.status).toBe('pending')
+        await game.start()
+        expect(game.status).toBe('in-progress')
     })
 });
