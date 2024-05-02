82e22b21527d1e713e4b10c300ec6a83afedb02f
MariyaN23 <sellaite505@gmail.com>
2024-05-02 22:03:03 +0300

    players position

--- game.js
+++ game.js
@@ -1,14 +1,15 @@
 class Game {
-    #settings
+    #settings = {
+        gridSize: {
+            x: 4,
+            y: 4
+        }
+    }
     #status = 'pending'
     #player1
     #player2
 
     constructor() {
-        this.#player1 = new Player(NumberUtil.getRandomNumber(0, this.#settings.gridSize.x),
-            NumberUtil.getRandomNumber(0, this.#settings.gridSize.y))
-        const randomPositionForPlayer2 = getRandomPosition()
-        this.#player2 = new Player(randomPositionForPlayer2.x, randomPositionForPlayer2.y)
     }
     #getRandomPosition() {
         let newX
@@ -17,7 +18,7 @@ class Game {
             newX = NumberUtil.getRandomNumber(0, this.#settings.gridSize.x)
             newY = NumberUtil.getRandomNumber(0, this.#settings.gridSize.y)
         } while (newX === this.#player1.x && newY === this.#player1.y)
-        return {x: newX, y: newY}
+        return new Position(newX, newY)
     }
     set settings(settings) {
         if (!settings.gridSize) {
@@ -31,18 +32,22 @@ class Game {
     get status() {
         return this.#status
     }
+    get players() {
+        return [this.#player1, this.#player2]
+    }
+    #createPlayers() {
+        const player1Position = new Position(NumberUtil.getRandomNumber(0, this.#settings.gridSize.x),
+            NumberUtil.getRandomNumber(0, this.#settings.gridSize.y))
+        this.#player1 = new Player(player1Position)
+        const player2Position = this.#getRandomPosition()
+        this.#player2 = new Player(player2Position)
+    }
     start() {
         if (this.#status === 'pending') {
+            this.#createPlayers()
             this.#status = 'in-progress'
         }
 
-    }
-}
-
-class Player {
-    constructor(x, y) {
-        this.x = x
-        this.y = y
     }
 }
 
@@ -51,6 +56,20 @@ class NumberUtil {
         return Math.floor(Math.random()*(max - min + 1)) + min
     }
 }
+
+class Position {
+    constructor(x, y) {
+        this.x = x
+        this.y = y
+    }
+}
+class Player {
+    constructor(position) {
+        this.position = position
+    }
+}
+
+
 
 module.exports = {
     Game
--- game.test.js
+++ game.test.js
@@ -20,4 +20,26 @@ describe('game test', ()=> {
         await game.start()
         expect(game.status).toBe('in-progress')
     })
+
+    it ('check player init position', async ()=> {
+        for (let i=0; i<10; i++) {
+            const game = new Game()
+            game.settings = {
+                gridSize: {
+                    x: 1,
+                    y: 2
+                }
+            }
+            game.start()
+            expect([0, 1]).toContain(game.players[0].position.x)
+            expect([0, 1]).toContain(game.players[0].position.y)
+
+            expect([0, 1]).toContain(game.players[1].position.x)
+            expect([0, 1]).toContain(game.players[1].position.y)
+
+            expect(game.players[0].position.x).not().toBe(game.players[1].position.x)
+            expect(game.players[0].position.y).not().toBe(game.players[1].position.y)
+        }
+    })
+
 });
