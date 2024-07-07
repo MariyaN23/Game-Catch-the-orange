1cbdc6672ffb941f1a8ce4e77367bb476ecf69f5
MariyaN23 <sellaite505@gmail.com>
2024-07-07 20:44:44 +0300

    controller nor only client mode

--- front.js
+++ front.js
@@ -1,9 +1,10 @@
-import {EventsFactory, Game} from './game.js';
+import {EventsFactory, Game, gameModes} from './game.js';
 import {EventEmitter} from './utils/observer/eventEmitter.js';
 import {GameComponent} from './view.js';
 import {Controller} from './controller.js';
 import {socket} from './front/ws.js';
 import {WSAdapter} from './front/WSAdapter.js';
+import {Controller1} from './controller1.js';
 
 const startGame = async () => {
     const eventEmitter = new EventEmitter()
@@ -11,11 +12,14 @@ const startGame = async () => {
     const wsAdapter = new WSAdapter(socket)
     const game = new Game("new game", eventEmitter, eventsFactory)
     await game.start()
-
-    const controller = new Controller(game, wsAdapter)
+    let controller
+    if (game.settings.mode === gameModes.onlyClient) {
+        controller = new Controller1(game)
+    } else {
+        controller = new Controller(game, wsAdapter)
+    }
     const view = new GameComponent(controller, game)
     view.render()
-
 }
 
 startGame()
